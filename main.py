2a2a399bb5ded5f0c9d10aa61d0cbe11a7d5c67f
Virginia Ceccatelli <157394080+virginiaceccatelli@users.noreply.github.com>
2024-07-02 14:46:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import seaborn as sns 
 import matplotlib.pyplot as plt
 import random
+import math as m
 import gym
 from gym import spaces 
 from sklearn.ensemble import RandomForestRegressor
@@ -19,10 +20,32 @@ from sklearn.preprocessing import StandardScaler
 file_path = r"C:\Users\4967\OneDrive - Wavestone Germany Group\Dokumente\problem_klein.xlsx"
 data = pd.read_excel(file_path)
 
-# Data formatting (NA to NaN) 
+# Data formatting  
 data.replace(["NA", "", "null", "Na", "N/A", "na"], np.nan, inplace=True)
-print(data.head())
+print(data)
 
+# VALIDITY: invalid values are stored in dictionary 
+invalid_indices_dict = {}
+for col_idx, column in enumerate(data.columns):
+    if pd.api.types.is_numeric_dtype(data[column]):
+        data[column] = pd.to_numeric(data[column], errors='coerce')  # convert numeric columns to float, coerce non-numeric to NaN
+        if ' age ' in column.lower():
+            data[column] = np.where((data[column] >= 0) & (data[column] <= 100), data[column], np.nan) # set invalid values for age as NaN
+    else:
+        for row_idx, value in data[column].items():
+            try:
+                float_value = float(value) # attempt to convert to float
+                if not m.isnan(float_value):
+                    if row_idx not in invalid_indices_dict: # store row and column index, as well as previous value, in dictionary if it succeeds
+                        invalid_indices_dict[row_idx] = []
+                    invalid_indices_dict[row_idx].append({
+                        'column': column,
+                        'previous_value': value,
+                    })
+                    data.at[row_idx, column] = np.nan # convert numeric value in non-numeric column to NaN 
+            except ValueError:
+                pass
+print(data) 
 # Encoding of categorical variables as numeric: decode after ML imputation to get original dataset 
 label_encoders = {}
 
@@ -38,7 +61,9 @@ for column in data.select_dtypes(include=['object']).columns:
 
 # Train 75%, Test 25%: after training, use unseen data in test subset to estimate algorithm accuracy and validity 
 train_data, test_data = train_test_split(data, test_size=0.25, random_state=0)
-print(data.head())
+print(data)
+print(invalid_indices_dict) 
+
 
 
 # Environment and Actions for RL
@@ -53,7 +78,7 @@ class Environment(gym.Env): # OpenAI Gym Environment Inheritance
         self.num_col = len(self.data.columns)
         self.num_row = len(self.data)
         self.inaccurate_numbers = []
-        self.invalid_numbers = []
+        self.majority_type = None
         self.previous_na = self.data.isna().sum().sum()
 
         # Action space describes the possible values that actions can take (finite) 
@@ -66,7 +91,7 @@ class Environment(gym.Env): # OpenAI Gym Environment Inheritance
         self.scaler = StandardScaler().fit(self.data.fillna(-9999)) # Standard Scaler cannot handle NaN values; they have to be transformed to out-of-bound number
         self.scaled_data = pd.DataFrame(self.scaler.transform(self.data.fillna(-9999)), columns=self.data.columns) # transform data while maintaining dataframe structure and column names
 
-        self.ocsvm = OneClassSVM(nu=0.01) # nu: upper bound on the fraction of margin errors and support vectors: at most 1% misinterpreted values
+        self.ocsvm = OneClassSVM(nu=0.01, gamma='scale', kernel='rbf') # nu: upper bound on the fraction of margin errors and support vectors: at most 1% misinterpreted values
         self.ocsvm.fit(self.scaled_data)
         
         # Initializing IterativeImputer 
@@ -104,7 +129,6 @@ class Environment(gym.Env): # OpenAI Gym Environment Inheritance
         
         done = self.current_col >= self.num_col 
         if done: 
-            self.data.convert_dtypes() # changes datatypes in dataframe to best fitting datatypes 
             self.finish = True 
             
         reward = self.calculate_reward()
@@ -119,30 +143,27 @@ class Environment(gym.Env): # OpenAI Gym Environment Inheritance
 
         return imputed_dataframe
 
-    # VALIDITY: extract majority type in column and check if value row_i, column_i is equivalent; if not it is appended to list invalid_numbers 
-    def check_validity(self, row_i, column_i): 
-        majority_dtype = self.data[column_i].dtypes
-        if self.data[row_i, column_i].dtype != majority_dtype: 
-            self.invalid_numbers.append(value)                    
-
     # ACCURACY: To replace inaccurate values (that are disproportionate/ outliers compared to the rest of the data) with predicted values
     def check_accuracy(self): 
         self.scaled_data = pd.DataFrame(self.scaler.transform(self.imputed_data), columns=self.data.columns) # maintain column names
 
         # One-Class SVM for outlier detection
+        self.ocsvm.set_params(nu=0.01)
         outliers_ocsvm = self.ocsvm.fit_predict(self.scaled_data)
         outliers = np.where(outliers_ocsvm == -1)[0] # finds outliers
 
-        # -- TO DO -- within given boundary (very obvious outliers), values are either changed through best fit prediction or appended to inaccurate_numbers list
+        self.data[:] = self.scaler.inverse_transform(self.scaled_data) # transform scaled data back to original scale
+
         for index in outliers:
-            row_index, col_index = divmod(index, self.num_col)
-            if pd.isna(self.data.iloc[row_index, col_index]) or self.data.iloc[row_index, col_index] == -9999: # re-impute if it's still NaN or -9999
-                self.inaccurate_numbers.append(self.data.iloc[row_index, col_index])
-                # self.data.iloc[row_index, col_index] = self.imputed_data.iloc[row_index, col_index] # replaces value of outlier with predicted value 
-
-        # Transform scaled data back to original scale
-        self.data[:] = self.scaler.inverse_transform(self.scaled_data)
-
+            row_index, col_index = divmod(index, self.num_col) # store index and previous value of outliers in list 
+            self.inaccurate_numbers.append({ 
+                'row': row_index,
+                'col': col_index,
+                'previous_value': self.data.iloc[row_index, col_index]
+            })
+        # -- TO DO (eventually) -- within given boundary (very obvious outliers), values are either changed through best fit prediction or appended to inaccurate_numbers list
+        # IDEA: increase nu parameter linear to exploration factor epsilon (nu increases proportional to exploration rate, so that the agent can learn the optimal nu parameter) 
+        
     # Calculate reward based on reduction of NaN, consistency with allowed data types, accuracy of data imputs (-- TO DO --)
     def calculate_reward(self):
         # Completeness: the less NaN values remaining, the higher the reward obtained
@@ -165,11 +186,10 @@ class Environment(gym.Env): # OpenAI Gym Environment Inheritance
         elif ocsvm_count != 0:
             outlier_reward = -ocsvm_count
 
-        # Validity: 
-        
         # Total reward 
         full_reward = na_reward + outlier_reward
         return full_reward
+
 
 class Agent: 
     def __init__(self, n_state, n_action): 
@@ -210,7 +230,7 @@ class Agent:
 
         # Temporal Difference Learning for Policy Evaluation 
         best_next_action = np.argmax(self.q_table[next_state_index]) # Greedy strategy for future state
-        td_prediction = reward + self.gamma * self.q_table[next_state_index][best_next_action] * (1 - done) # TD prediction formula: reward + discounting rate * max Q(S(t+1), A), (1 - done) to ignore terminal rewards that are irrelevant
+        td_prediction = reward + (self.gamma * self.q_table[next_state_index][best_next_action] * (1 - done)) # TD prediction formula: reward + discounting rate * max Q(S(t+1), A), (1 - done) to ignore terminal rewards that are irrelevant
         td_error = td_prediction - self.q_table[state_index][action] # TD error formula: TD prediction - current Q(S, A) 
         self.q_table[state_index][action] += self.lr * td_error[action] # TD learning formula: Q(S, A) + TD error * learning rate (alpha)
 
@@ -239,17 +259,11 @@ if __name__ == "__main__":
             agent.learn(state, action, reward, next_state, done) # SARS -> Q learning 
             state = next_state
             total_reward += reward
-            print(env.data)
-
-            print(f"Episode: {episode + 1}, Iteration: {env.iteration}, Column: {env.current_col}, Reward: {reward}")
-
-            if done: 
-                print(f"Episode: {episode + 1}, Total Reward: {total_reward}, Exploration Rate: {agent.epsilon}, dtype: {env.data.dtypes}")
-                np.round(env.data)
-                print(env.data)
-                break
+            print(f"Episode: {episode + 1}, Row: {env.iteration}, Column: {env.current_col}, Reward: {reward}")
 
             if done: 
                 print(f"Episode: {episode + 1}, Total Reward: {total_reward}, Exploration Rate: {agent.epsilon}")
+                np.round(env.data)
+                print(f"Invalid Numbers: {invalid_indices_dict}, Inaccurate Numbers: {env.inaccurate_numbers}")
                 print(env.data)
                 break
