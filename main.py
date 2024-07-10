769d458361d78f4b2e2702f071b61d8c1da7c20f
Virginia Ceccatelli <157394080+virginiaceccatelli@users.noreply.github.com>
2024-07-10 11:51:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ import gym
 from gym import spaces 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.impute import IterativeImputer, SimpleImputer, KNNImputer
 from sklearn import metrics
+from sklearn.metrics import mean_squared_error
 from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.svm import OneClassSVM
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 file_path = r"C:\Users\4967\OneDrive - Wavestone Germany Group\Dokumente\problem_klein.xlsx"
-data = pd.read_excel(file_path)
+solved_file_path = r"C:\Users\4967\OneDrive - Wavestone Germany Group\Dokumente\solved_klein.xlsx"
+data = pd.read_excel(file_path, header=0)
+
+solved_data = pd.read_excel(solved_file_path)
 
 # Data formatting  
 data.replace(["NA", "", "null", "Na", "N/A", "na"], np.nan, inplace=True)
@@ -59,6 +62,7 @@ for column in data.columns:
             except ValueError:
                 pass
 print(data) 
+
 # Encoding of categorical variables as numeric: decode after ML imputation to get original dataset 
 ordinal_encoders = {}
 for column in data.select_dtypes(include=['object']).columns:
@@ -72,6 +76,14 @@ for column in data.select_dtypes(include=['object']).columns:
     data[column] = series_copy
     ordinal_encoders[column] = ordinal_encoder
 
+# Encoding solved dataset
+for column in solved_data.select_dtypes(include=['object']).columns:
+    if column in ordinal_encoders:
+        ordinal_encoder = ordinal_encoders[column]
+        series = solved_data[column]
+        encoded_values = ordinal_encoder.transform(series.values.reshape(-1, 1)).flatten()
+        solved_data[column] = encoded_values
+    
 # Train 75%, Test 25%: after training, use unseen data in test subset to estimate algorithm accuracy and validity 
 train_data, test_data = train_test_split(data, test_size=0.25, random_state=0, shuffle=False)
 print(data)
@@ -82,20 +94,21 @@ print(invalid_indices_dict)
 
 # Environment and Actions for RL
 class Environment(gym.Env): # OpenAI Gym Environment Inheritance 
-    def __init__(self, data):
+    def __init__(self, data, solved_data):
         super(Environment, self).__init__()
         self.data = data
+        self.solved_data = solved_data
         self.original_data = self.data.copy()
         self.iteration = 0
         self.finish = False
         self.current_col = 0
-        self.num_col = len(self.data.columns)
-        self.num_row = len(self.data)
+        self.num_col = len(self.data.columns) 
+        self.num_row = len(self.data) 
         self.inaccurate_numbers = []
         self.previous_na = self.data.isna().sum().sum()
 
         # Action space describes the possible values that actions can take (finite) 
-        self.action_space = spaces.Discrete(self.num_row)
+        self.action_space = spaces.Discrete(3)
 
         # Observation space (Box) describes valid values that observations can take for consistent state representation  
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(self.num_row, self.num_col), dtype=np.float32) 
@@ -107,8 +120,11 @@ class Environment(gym.Env): # OpenAI Gym Environment Inheritance
         self.ocsvm = OneClassSVM(nu=0.01, gamma='scale', kernel='rbf') # nu: upper bound on the fraction of margin errors and support vectors: at most 1% misinterpreted values
         self.ocsvm.fit(self.scaled_data)
         
-        # Initializing IterativeImputer 
+        # Imputation Techniques 
         self.imputer = IterativeImputer(estimator=RandomForestRegressor(), max_iter=10, random_state=0)
+        self.imputer_knn = KNNImputer(n_neighbors=5)
+        self.imputer_mean = SimpleImputer(strategy='mean')
+
 
     # Reset dataset back to original values after each episode of training 
     def reset(self):
@@ -117,44 +133,50 @@ class Environment(gym.Env): # OpenAI Gym Environment Inheritance
         self.current_col = 0 
         self.previous_na = self.data.isna().sum().sum()
         self.imputed_data = self.imputer.fit_transform(self.data)
+        self.imputed_data_knn = self.imputer_knn.fit_transform(self.data)
+        self.imputed_data_mean = self.imputer_mean.fit_transform(self.data)
         self.scaled_data = pd.DataFrame(self.scaler.transform(self.imputed_data), columns=self.data.columns)
         return self.data.values.flatten() 
 
     # Iterate over each row in every column and apply impute_value where NaN, check_accuracy to all and calculate_reward when done with episode
     def step(self, action): 
-        row_i = self.iteration + 1
-        column_i = self.current_col + 1
-        if row_i < self.num_row and column_i < self.num_col :
+        row_i = self.iteration 
+        column_i = self.current_col
+        done = self.current_col == self.num_col - 1 and self.iteration == self.num_row - 1
+        if column_i < self.num_col:
             if self.data.iloc[row_i, column_i] == -9999 or pd.isna(self.data.iloc[row_i, column_i]): # Look for NaN values (or that were replaced with -9999)
                 try:
-                    imputed_dataframe = self.impute_value()
-                    self.data.iloc[row_i, column_i] = imputed_dataframe.iloc[row_i, column_i]
+                    self.data.iloc[row_i, column_i] = self.impute_value(action, row_i, column_i)    
                 except (TypeError, ValueError):
                     pass
-                
+                    
             self.check_accuracy() # check if value is accurate (outliers)
-            self.iteration += 1
-                
-            # Move to next column 
-            if self.iteration >= self.num_row:
-                self.iteration = 0 
-                self.current_col += 1
-            
-        done = self.current_col >= self.num_col and self.iteration >= self.num_row
+
         if done: 
-            self.finish = True 
-        reward = self.calculate_reward()
+            self.finish = True
+            reward = self.calculate_reward(action, row_i, column_i)
+            return self.data.values.flatten(), reward, done, {}
         
+        # Move to next row
+        reward = self.calculate_reward(action, row_i, column_i)
+        self.iteration += 1
+            
+        # Move to next column 
+        if self.iteration >= self.num_row:
+            self.iteration = 0 
+            self.current_col += 1
         return self.data.values.flatten(), reward, done, {}
         
-    # COMPLETENESS: Impute missing values based on ML imputation method 'Iterative Imputer' using 'Random Forest Regressor' 
-    def impute_value(self):        
+    # COMPLETENESS: Impute missing values based on ML imputation method 'Iterative Imputer' using 'Random Forest Regressor', 'K Nearest Neighbour' or the Mean
+    def impute_value(self, action, row_i, column_i):        
         self.data.replace(-9999, np.nan, inplace=True)
-        imputed_data = self.imputer.fit_transform(self.data)
-        imputed_dataframe = pd.DataFrame(imputed_data, columns=self.data.columns)
-
-        return imputed_dataframe
-
+        if action == 0:
+            return self.imputed_data_rf[row_i, column_i]
+        elif action == 1:
+            return self.imputed_data_knn[row_i, column_i]
+        elif action == 2:
+            return self.imputed_data_mean[row_i, column_i]
+        
     # ACCURACY: To replace inaccurate values (that are disproportionate/ outliers compared to the rest of the data) with predicted values
     def check_accuracy(self): 
         self.scaled_data = pd.DataFrame(self.scaler.transform(self.imputed_data), columns=self.data.columns) # maintain column names
@@ -187,18 +209,23 @@ class Environment(gym.Env): # OpenAI Gym Environment Inheritance
         # IDEA: increase nu parameter linear to exploration factor epsilon (nu increases proportional to exploration rate, so that the agent can learn the optimal nu parameter) 
         
     # Calculate reward based on reduction of NaN, consistency with allowed data types, accuracy of data imputs (-- TO DO --)
-    def calculate_reward(self):
-        # Completeness: the less NaN values remaining, the higher the reward obtained
-        if self.current_col >= self.num_col: 
-            na_reward = -self.data.isna().sum().sum()
-        else:
-            current_na = self.data.iloc[:, self.current_col].isna().sum()
-            if current_na > 0:
-                na_reward = -current_na
-            else:
-                na_reward = 1
+    def calculate_reward(self, action, row_i, column_i):
+        # Completeness: the less NaN values remaining and the closer they are to the solved dataset, the higher the reward
+        current_value = self.data.iloc[row_i, column_i]
+        solved_value = self.solved_data.iloc[row_i, column_i]
+        
+        if pd.isnull(current_value) or current_value == -9999:  # Handle NaN or imputed values
+            return 0  # No reward for missing values or already imputed values
 
-        return na_reward
+        print(f"Imputed Value: {current_value} and Solved Value: {solved_value}")
+        diff = abs(current_value - solved_value)
+        # Reward based on the closeness of the imputed value to the solved value
+        if diff == 0:
+            reward = 100  # Maximum reward for exact match
+        else:
+            reward = 100 / (diff + 1)  # Reward decreases as the difference increases
+    
+        return reward
 
 
 class Agent: 
@@ -239,11 +266,11 @@ class Agent:
         action = int(action) # convert action to integer 
 
         # Temporal Difference Learning for Policy Evaluation 
-        best_next_action = np.argmax(self.q_table[next_state_index]) # Greedy strategy for future state
-        td_prediction = reward + (self.gamma * self.q_table[next_state_index][best_next_action] * (1 - done)) # TD prediction formula: reward + discounting rate * max Q(S(t+1), A), (1 - done) to ignore terminal rewards that are irrelevant
+        best_next_action = np.argmax(self.q_table[state_index]) # Greedy strategy for future state
+        td_prediction = reward + (self.gamma * self.q_table[state_index][best_next_action] * (1 - done)) # TD prediction formula: reward + discounting rate * max Q(S(t+1), A), (1 - done) to ignore terminal rewards that are irrelevant
         td_error = td_prediction - self.q_table[state_index][action] # TD error formula: TD prediction - current Q(S, A) 
-        self.q_table[state_index][action] += self.lr * td_error[action] # TD learning formula: Q(S, A) + TD error * learning rate (alpha)
-
+        self.q_table[state_index][action] += self.lr * td_error # TD learning formula: Q(S, A) + TD error * learning rate (alpha)
+    
         # decrease exploration to encourage exploitation with each episode 
         if done: 
             self.epsilon = max(self.min_epsilon, self.epsilon * self.gamma) # choose max value between epsilon * discounting rate and 0.01 (convergence towards exploitation)
@@ -251,21 +278,23 @@ class Agent:
     def update_state(self, state):
         self.current_state = self.state_to_index(state) # state index updated 
 
+
 if __name__ == "__main__":
-    env = Environment(train_data)
+    env = Environment(train_data, solved_data)
     agent = Agent(n_state=env.observation_space.shape[0], n_action=env.action_space.n)
     
     episodes = 1
     for episode in range(episodes):
         state = env.reset()
-        reward = env.calculate_reward()
         agent.update_state(state)
         total_reward = 0
         
         while True:
             action = agent.take_action(state)
             next_state, reward, done, _ = env.step(action)
-            agent.learn(state, action, reward, next_state, done) # SARS -> Q learning 
+            reward = env.calculate_reward(action, env.iteration, env.current_col)
+            agent.learn(state, next_state, action, reward, done) # SARS -> Q learning 
+            
             state = next_state
             total_reward += reward
             print(f"Episode: {episode + 1}, Row: {env.iteration}, Column: {env.current_col}, Reward: {reward}")
